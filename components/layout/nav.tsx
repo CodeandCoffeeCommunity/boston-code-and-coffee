f5a77d3d8ed237772585601e19067483ebc3aaee
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-06-19 19:41:09 -0400

    reworked folder structure to allow for better routing

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,9 +1,12 @@
 import Logo from "./Logo";
+import Link from "next/link";
 
 export default function Nav() {
   return (
     <nav className="mx-auto max-w-7xl px-5 mb-9">
-      <Logo />
+      <Link href={"/"}>
+        <Logo />
+      </Link>
       <h2 className="text-4xl font-bold text-gray-900">Boston</h2>
     </nav>
   );
