4401db76d5c739d22a959b866c21ddba3e6d0f52
Alex Pereira <alex.pereira@outlook.com>
2023-08-03 19:31:58 -0400

    Update MeetupGroup

--- app/types/domain/MeetupGroup.ts
+++ app/types/domain/MeetupGroup.ts
@@ -2,5 +2,11 @@ import { MeetupEvent } from './MeetupEvent';
 
 export type MeetupGroup = {
   id: string;
+  name?: string;
+  city?: string;
+  state?: string;
+  urlname?: string;
+
   pastEvents: MeetupEvent[] | null;
+  upcomingEvents: MeetupEvent[] | null;
 };
