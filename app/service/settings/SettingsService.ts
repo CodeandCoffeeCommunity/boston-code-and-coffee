b6b05e00aaf1899bb745948a973343942d089993
Alex Pereira <alex.pereira@outlook.com>
2023-08-03 23:58:05 -0400

    Catch error response in SettingsService

--- app/service/settings/SettingsService.ts
+++ app/service/settings/SettingsService.ts
@@ -7,11 +7,18 @@ import { request } from '@/app/util/RequestUtil';
  * Get a list of all the Code and Coffee Chapters
  */
 export async function getCodeCoffeeChapters(): Promise<CodeCoffeeChapter[]> {
-  return await request({
-    name: 'Chapters Setting',
-    url: `${configuration.SETTINGS_BASE_URL}/chapters.json`,
-    method: 'GET',
-  }) as CodeCoffeeChapter[];
+  let response: CodeCoffeeChapter[];
+  try {
+    response = await request({
+      name: 'Chapters Setting',
+      url: `${configuration.SETTINGS_BASE_URL}/chapters.json`,
+      method: 'GET',
+    }) as CodeCoffeeChapter[];
+  } catch (error) {
+    return [];
+  }
+
+  return response;
 }
 
 /**
@@ -20,10 +27,17 @@ export async function getCodeCoffeeChapters(): Promise<CodeCoffeeChapter[]> {
  * @param chapter The chapter to get the icon for
  */
 export async function getChapterIcon(chapter: string): Promise<ArrayBuffer> {
-  return await request({
-    name: 'Chapter Icon',
-    responseType: 'arrayBuffer',
-    url: `${configuration.SETTINGS_BASE_URL}/chapter-icons/${chapter}.png`,
-    method: 'GET',
-  });
+  let response: ArrayBuffer;
+  try {
+    response = await request({
+      name: 'Chapter Icon',
+      responseType: 'arrayBuffer',
+      url: `${configuration.SETTINGS_BASE_URL}/chapter-icons/${chapter}.png`,
+      method: 'GET',
+    });
+  } catch (error) {
+    return new ArrayBuffer(0);
+  }
+
+  return response;
 }
