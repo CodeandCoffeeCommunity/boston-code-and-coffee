789a6942e3da10c298a37db9c55a744f26674c85
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-08-12 08:13:56 -0400

    added content

--- app/(routes)/about/faqs/page.tsx
+++ app/(routes)/about/faqs/page.tsx
@@ -1,6 +1,38 @@
+import Wrapper from "@/components/layout/Wrapper"
+import Button from "@/components/shared/Button"
+
 export default function AboutFaQs() {
   return (
-    <div className="my-2 flex flex-col justify-evenly">
-  </div>
+    <Wrapper>
+      <h4 className="text-xl mb-4 font-bold leading-none tracking-tight text-gray-900">
+        Frequently Asked Questions
+      </h4>
+      <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+        How can I get involved?
+      </h5>
+        <p>
+          If you wish to volunteer to Boston Code and Coffee lease reach out to us on bostoncodeandcoffee@gmail.com and we will set up an initial call to discuss your participation. 
+          You can also volunteer to host a feature
+        </p>
+        <Button
+          title={"Feature Sign Up Page"}
+          link={"/features"}
+          color={"bg-yellow-400"}
+          size={"small"}
+        />
+      <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+        How can my organization or business I work for help out?
+      </h5>
+        <p>
+          We’re always looking for venue and coffee sponsors.
+          Send us an email at sponsor@codeandcoffee.org
+        </p>
+        <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+        I want to report someone in violation of the Code of Conduct. Where can I report?
+      </h5>
+        <p>
+          Email us at safety@codeandcoffee.org, we strive to get back to you within 24 hours.
+        </p>
+    </Wrapper>
   )
 }
--- app/(routes)/about/info/page.tsx
+++ app/(routes)/about/info/page.tsx
@@ -3,9 +3,41 @@ import Wrapper from "@/components/layout/Wrapper"
 export default function AboutInfo() {
   return (
     <Wrapper>
-      <h3 className="text-2xl mb-4 font-extrabold leading-none tracking-tight text-gray-900">
-        What are features?
-      </h3>
+      <h4 className="text-xl mb-4 font-bold leading-none tracking-tight text-gray-900">
+        What is Code and Coffee?
+      </h4>
+      <p>
+      🧑‍🤝‍🧑🧑‍🤝‍🧑 We’re a Community. Our attendees feel like they’re a part of the group. 
+      This group is not run by a company -- it is ran by a group of individual community members.
+      </p>
+      <p>
+      📈🔍 Of developers. Or for people who work with them or want to become one.
+      </p>
+      <p>
+      🐣🐓 At Any Level. Whether you’re aspiring, newer, or more experienced.
+      </p>
+      <p>
+      We invite anyone working on software development, especially new devs and folks that work with them. 
+      You may already be working in one of these roles, or you might be interested in learning more about these roles — either way, you’re welcome!
+      </p>
+
+      <h4 className="text-xl mb-4 font-bold leading-none tracking-tight text-gray-900">
+      What do we do at these events?
+      </h4>
+
+      <p>
+      💬 Discussion. Many attendees come with questions, ideas, or strategies to discuss.
+      </p>
+
+      <p>
+      🕸️ Networking. Many people come to meet people, that's enough reason on its own.
+      </p>
+
+      <p>
+      🫲🏻 Support. Many of our attendees in the past have also found mentors, mentees, and peer-support. 
+      We encourage it! Many people come with questions, ideas or strategies to discuss — and many others just come to meet people! 
+      Many of our attendees in the past have also found mentors, mentees, and peer-support friends — and we encourage attendees to form relationships like these.
+      </p>
     </Wrapper>
   )
 }
