fba8902cbc460fad7798c3d5b81dc9d90e332005
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-08-17 08:34:39 -0400

    WIP

--- app/(routes)/about/info/page.tsx
+++ app/(routes)/about/info/page.tsx
@@ -1,43 +1,41 @@
-import Wrapper from "@/components/layout/Wrapper"
-
 export default function AboutInfo() {
   return (
-    <Wrapper>
-      <h4 className="text-xl mb-4 font-bold leading-none tracking-tight text-gray-900">
+    <div className="px-7">
+      <h4 className="text-xl font-bold leading-none tracking-tight text-gray-900">
         What is Code and Coffee?
       </h4>
-      <p>
+      <p className="pt-4">
       🧑‍🤝‍🧑🧑‍🤝‍🧑 We’re a Community. Our attendees feel like they’re a part of the group. 
       This group is not run by a company -- it is ran by a group of individual community members.
       </p>
-      <p>
+      <p className="pt-4">
       📈🔍 Of developers. Or for people who work with them or want to become one.
       </p>
-      <p>
+      <p className="pt-4">
       🐣🐓 At Any Level. Whether you’re aspiring, newer, or more experienced.
       </p>
-      <p>
+      <p className="pt-4">
       We invite anyone working on software development, especially new devs and folks that work with them. 
       You may already be working in one of these roles, or you might be interested in learning more about these roles — either way, you’re welcome!
       </p>
 
-      <h4 className="text-xl mb-4 font-bold leading-none tracking-tight text-gray-900">
+      <h4 className="text-xl pt-4 font-bold leading-none tracking-tight text-gray-900">
       What do we do at these events?
       </h4>
 
-      <p>
+      <p className="pt-4">
       💬 Discussion. Many attendees come with questions, ideas, or strategies to discuss.
       </p>
 
-      <p>
+      <p className="pt-4">
       🕸️ Networking. Many people come to meet people, that's enough reason on its own.
       </p>
 
-      <p>
+      <p className="pt-4">
       🫲🏻 Support. Many of our attendees in the past have also found mentors, mentees, and peer-support. 
       We encourage it! Many people come with questions, ideas or strategies to discuss — and many others just come to meet people! 
       Many of our attendees in the past have also found mentors, mentees, and peer-support friends — and we encourage attendees to form relationships like these.
       </p>
-    </Wrapper>
+    </div>
   )
 }
--- app/(routes)/about/principals/page.tsx
+++ app/(routes)/about/principals/page.tsx
@@ -1,33 +1,31 @@
-import Wrapper from "@/components/layout/Wrapper"
-
 export default function AboutPrincipals() {
   return (
-    <Wrapper>
-      <h4 className="text-xl mb-4 font-bold leading-none tracking-tight text-gray-900">
+    <div className="px-7">
+      <h4 className="text-xl pb-4 font-bold leading-none tracking-tight text-gray-900">
         Our Community Principals
       </h4>
-      <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+      <h5 className="text-base font-bold leading-none tracking-tight text-gray-900">
         Belonging
       </h5>
         <p>
           We strive for everyone to feel welcomed, valued, and connected.
           We believe in encouraging belonging through conscious diversity and inclusion, 
-          especially amongst underrepresented groups in tech. Check out our staff page!
+          especially amongst underrepresented groups in tech. Check out our <span className="font-bold underline">staff</span> page!
         </p>
-      <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+      <h5 className="text-base pt-4 font-bold leading-none tracking-tight text-gray-900">
         Safety
       </h5>
         <p>
           We believe in building trust and maintaining a secure environment in our community.
-          We take safety seriously and expect all participants to abide by our Code of Conduct
+          We take safety seriously and expect all participants to abide by our <span className="font-bold underline">Code of Conduct</span>
         </p>
-      <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+      <h5 className="text-base pt-4 font-bold leading-none tracking-tight text-gray-900">
         Trust
       </h5>
         <p>
           We strive to foster trust and build relationships by creating a community based on honesty, reliability, and integrity.
         </p>
-      <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+      <h5 className="text-base pt-4 font-bold leading-none tracking-tight text-gray-900">
         Choose-Your-Own-Adventure
       </h5>
         <p>
@@ -35,31 +33,30 @@ export default function AboutPrincipals() {
           Or force you to do anything that goes against what you want to do. 
           You are free to come, sip some coffee, and go off on your own adventure.
         </p>
-        <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+        <h5 className="text-base pt-4 font-bold leading-none tracking-tight text-gray-900">
         Open
       </h5>
         <p>
           We believe in community-led communities where every voice is heard and respected—embracing authenticity and transparency in our community.
         </p>
-        <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+        <h5 className="text-base pt-4 font-bold leading-none tracking-tight text-gray-900">
         Consistent
       </h5>
         <p>
         We strive for consistency in all we do—providing a dependable experience for our community. We aim for 1 or 2 events per month, depending on organizers and venue availability.
         </p>
-        <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+        <h5 className="text-base pt-4 font-bold leading-none tracking-tight text-gray-900">
         Local Autonomy
       </h5>
         <p>
         We support giving voice to the unique needs of each geographically local community.
         </p>
-        <h5 className="text-base mb-4 font-bold leading-none tracking-tight text-gray-900">
+        <h5 className="text-base pt-4 font-bold leading-none tracking-tight text-gray-900">
         Fun In Everything we do
       </h5>
         <p>
         we aim to stay true to the human-spirit of connection. We strive for our events to be a fun place to make friends and grow!
         </p>
-      
-    </Wrapper>
+    </div>
   )
 }
